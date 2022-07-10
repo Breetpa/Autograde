1e675810441114e97d8282d6be5cbb8584ddebb1
Breetpa <105613965+Breetpa@users.noreply.github.com>
2022-07-10 09:55:40 -0400

    init project

---
+++ src/test/java/com/autograde/ag/AutoGradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autograde.ag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoGradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

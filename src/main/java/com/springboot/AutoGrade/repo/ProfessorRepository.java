83f5d10023034c78ad62afb4ab56756dde02afac
Breetpa <105613965+Breetpa@users.noreply.github.com>
2022-07-11 22:30:13 -0400

    no error yet

--- src/main/java/com/springboot/AutoGrade/repo/ProfessorRepository.java
+++ src/main/java/com/springboot/AutoGrade/repo/ProfessorRepository.java
@@ -1,17 +1,10 @@
 package com.springboot.AutoGrade.repo;
 
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 
 import com.springboot.AutoGrade.Model.Professor;
 
-public interface ProfessorRepository extends JpaRepository<Professor, Integer> {
+public interface ProfessorRepository extends CrudRepository<Professor, Integer> {
 
-    @Query("from User where email=?1")
-    public List<Professor> findByEMAIL(String email);
-
-    @Query("from User where email=?1 and user_pass=?2")
-    public Professor findByEmailPassword(String email, String password);
-
+    
 }
